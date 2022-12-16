a8ecd35adf14537e77929dff3f9d9cc134e9c0d6
shafayetShafee <seaborn.sicario@gmail.com>
2022-12-16 19:23:01 +0600

    corrected a typo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastappi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import pickle
 import json
